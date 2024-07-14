737ff4b7c8c1f4c1178a162bc43b9500458fb27f
conradoMay <166981867+conradoMay@users.noreply.github.com>
2024-07-13 19:00:42 -0600

    iniiando practica

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-center font-bold">Mi primer ejemplo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
